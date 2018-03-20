515bc6958ecc40ed3e998fa8f45a7787ad3d7c7a
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2018-03-20 12:41:21 +0100

    add an latency extractor script for easier benchmarking

---
+++ extract_latency.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+outputs=*output.txt
+
+for file in $outputs; do
+    partition=$(echo $file | egrep -o '^[0-9]+')
+    latency=$(grep -A 1 '^LATENCY' $file | tail -n 1 | grep -oP '^([^,]+)')
+    echo "partition ${partition} latency: ${latency}"
+done
