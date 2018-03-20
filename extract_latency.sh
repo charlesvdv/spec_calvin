9ca5f96224b8b1b9102b60e370dd0781ee8fe27d
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2018-03-20 12:43:26 +0100

    improve sorting capabilities

--- extract_latency.sh
+++ extract_latency.sh
@@ -7,5 +7,5 @@ outputs=*output.txt
 for file in $outputs; do
     partition=$(echo $file | egrep -o '^[0-9]+')
     latency=$(grep -A 1 '^LATENCY' $file | tail -n 1 | grep -oP '^([^,]+)')
-    echo "partition ${partition} latency: ${latency}"
+    echo "${partition} latency: ${latency}"
 done
