1f888720ef27519f80630b6ca27a46e68e53347d
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2018-04-29 18:03:35 +0200

    fix extract_latency.sh

--- extract_latency.sh
+++ extract_latency.sh
@@ -7,7 +7,7 @@ outputs=*output.txt
 for file in $outputs; do
     if [[ -s $file ]]; then
         partition=$(echo $file | egrep -o '^[0-9]+')
-        latency=$(grep -A 1 '^LATENCY' $file | tail -n 1 | grep -oP '^([^,]+)')
+        latency=$(grep -A 1 '^LATENCY$' $file | tail -n 1 | grep -oP '^([^,]+)')
         echo "${partition} latency: ${latency}"
     fi
 done
